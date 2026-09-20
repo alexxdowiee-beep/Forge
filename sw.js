c67cc4432c909d86ed90c2ebe319dab5ec424f3a
alexxdowiee-beep <alexxdowiee@gmail.com>
2026-09-20 22:07:40 +1200

    Bump cache to v11

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Forge service worker - offline support
-const CACHE = 'forge-v10';
+const CACHE = 'forge-v11';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
